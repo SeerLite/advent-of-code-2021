cdaa6cc366a4c3a29593c2819d39a8621b73da05
SeerLite <seerlite@tuta.io>
2021-12-04 01:15:36 -0300

    Day 3: Move all of part 1 to its own function

--- 3/main.scm
+++ 3/main.scm
@@ -11,56 +11,64 @@
                             (with-input-from-file "input.txt"
                                                   (lambda () (get-string-all (current-input-port))))
                             #\newline)))
-(define ones
-        (let ((results (make-list (length (car inputs)) 0)))
-           (fold (lambda (current-line results)
-                         (map (lambda (val char)
-                                      (if (eqv? char #\1)
-                                         (1+ val)
-                                         val))
-                              results
-                              current-line))
-                 results
-                 inputs)))
-(define zeroes
-        (let ((total-lines (length inputs)))
-             (map (lambda (this-one)
-                          (- total-lines this-one))
-                  ones)))
 
 (define (print-num-list name content)
         (let ((content (string-join (map number->string content))))
              (format #t "~a: ~a\n" name content)))
 
-(print-num-list "ones" ones)
-(print-num-list "zeroes" zeroes)
-
-(define gamma (map (lambda (ones zeroes)
-                           (if (> ones zeroes)
-                               1
-                               0))
-                   ones
-                   zeroes))
-
-(define epsilon (map (lambda (n)
-                             (logxor n 1))
-                     gamma))
-
-(print-num-list "gamma" gamma)
-(print-num-list "epsilon" epsilon)
-
 (define (bin-list->integer bin-list)
-        (string->number (string-join (map number->string
-                                          bin-list)
-                                     "")
-                        2))
+       (string->number (string-join (map number->string
+                                         bin-list)
+                                    "")
+                       2))
 
-(define gamma-as-number (bin-list->integer gamma))
-(define epsilon-as-number (bin-list->integer epsilon))
+(define
+ (part-1)
+ (define ones
+         (let ((results (make-list (length (car inputs)) 0)))
+            (fold (lambda (current-line results)
+                          (map (lambda (val char)
+                                       (if (eqv? char #\1)
+                                          (1+ val)
+                                          val))
+                               results
+                               current-line))
+                  results
+                  inputs)))
+ (define zeroes
+         (let ((total-lines (length inputs)))
+              (map (lambda (this-one)
+                           (- total-lines this-one))
+                   ones)))
 
-(format #t "gamma as number: ~a\n"
-        gamma-as-number)
-(format #t "epsilon as number: ~a\n"
-        epsilon-as-number)
-(format #t "product of gamma and epsilon: ~a\n"
-        (* gamma-as-number epsilon-as-number))
+ (print-num-list "ones" ones)
+ (print-num-list "zeroes" zeroes)
+
+ (define gamma (map (lambda (ones zeroes)
+                            (if (> ones zeroes)
+                                1
+                                0))
+                    ones
+                    zeroes))
+
+ (define epsilon (map (lambda (n)
+                              (logxor n 1))
+                      gamma))
+
+ (print-num-list "gamma" gamma)
+ (print-num-list "epsilon" epsilon)
+
+ (define gamma-as-number (bin-list->integer gamma))
+ (define epsilon-as-number (bin-list->integer epsilon))
+
+ (format #t "gamma as number: ~a\n"
+         gamma-as-number)
+ (format #t "epsilon as number: ~a\n"
+         epsilon-as-number)
+ (format #t "product of gamma and epsilon: ~a\n"
+  (* gamma-as-number epsilon-as-number)))
+
+(define (part-2) #t)
+
+(part-1)
+(part-2)
