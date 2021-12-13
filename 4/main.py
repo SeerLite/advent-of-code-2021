a3f0a90c3b207a3e339cda91a48da780aa843036
SeerLite <seerlite@tuta.io>
2021-12-13 15:49:01 -0300

    Day 4: Add code for part 1

---
+++ 4/main.py
@@ -0,0 +1,97 @@
+# I'm gonna try to catch up to the newer puzzles as fast as possible
+# so don't expect the best code.
+# I'll be treating all these numbers as strings.
+#
+# Maybe I learn something about avoiding premature optimization.
+
+from typing import List, Tuple, Dict, Any, Optional
+
+# boards = [
+#     [ # board
+#         {"2": False, "28": False, "33": False}, # row
+#         {"11": False, "99": False, "21": False}, # row
+#         {"2": False, "28": False, "33": False} # row
+#     ],
+#     [ # board
+#         {"8": False, "9": False, "42": False} # row
+#         ...
+#     ]
+#     ...
+# ]
+
+Board = List[Dict[str, bool]]
+
+def read_input_file() ->  Tuple[str, str]:
+    with open("input.txt") as input_file:
+        order = input_file.readline().strip()
+
+        # Empty line separator
+        input_file.readline()
+
+        boards = input_file.read().strip()
+
+        return order, boards
+
+def parse_raw_boards(raw_boards: str) -> List[Board]:
+    boards = []
+
+    # Spiltting at every double newline will split at every board
+    # separation, as they're separated by empty lines
+    for raw_board in raw_boards.split("\n\n"):
+        board = []
+        for raw_row in raw_board.split("\n"):
+            row = {}
+            for number in raw_row.split():
+                row[number] = False
+            board.append(row)
+        boards.append(board)
+
+    return boards
+
+def board_wins_row(board: Board) -> bool:
+    return any(all(row.values()) for row in board)
+
+def board_wins_column(board: Board) -> bool:
+    board_size = len(board[0])
+    for column in range(board_size):
+        if all(list(board[row].values())[column] for row in range(board_size)):
+            return True
+
+    return False
+
+def find_winning_board(boards: List[Board]) -> Optional[int]:
+    for i, board in enumerate(boards):
+        if board_wins_row(board) or board_wins_column(board):
+            return i
+
+    return None # should be implied but mypy complains
+
+def play_boards(boards: List[Board]) -> Tuple[Board, int]:
+    while not (winning_index := find_winning_board(boards)):
+        number = order.pop()
+        for board in boards:
+            for row in board:
+                if number in row:
+                    row[number] = True
+
+    return boards[winning_index], int(number)
+
+def sum_all_unmarked(board: Board) -> int:
+    sum = 0
+    for row in board:
+        for number, marked in row.items():
+            if not marked:
+                sum += int(number)
+
+    return sum
+
+raw_order, raw_boards = read_input_file()
+order = raw_order.split(",")
+order.reverse()
+boards = parse_raw_boards(raw_boards)
+
+winning_board, winning_number = play_boards(boards)
+
+score = sum_all_unmarked(winning_board) * winning_number
+
+print(f"Score of winning board: {score}")
