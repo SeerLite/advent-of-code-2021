d5097080d5a872c8290a604a00cb0f8b778c1625
SeerLite <seerlite@tuta.io>
2021-12-14 23:25:15 -0300

    Day 6: Make progress on part 2

--- 6/main.py
+++ 6/main.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Sequence
 
-EXAMPLE = False
+EXAMPLE = True
 DEBUG = True
 
 def read_list_from_file() -> List[int]:
@@ -12,23 +12,53 @@ def read_list_from_file() -> List[int]:
     with open(filename) as input_file:
         return [int(x) for x in input_file.read().strip().split(",")]
 
-def pass_days(lanternfish_input: List[int], days: int) -> List[int]:
-    lanternfish = lanternfish_input.copy()
-    for day in range(days):
-        for i in range(len(lanternfish)):
-            lanternfish[i] -= 1
-            if lanternfish[i] < 0:
-                lanternfish[i] = 6
-                lanternfish.append(8)
+def pass_days(lanternfish_input: Sequence[int], days: int) -> Sequence[int]:
+    if DEBUG:
+        print(f"DAYS: {days}")
+    lanternfish = [x for x in lanternfish_input]
+    # OK so what if I immediately subtract days from all of them and then add accordingly
+    for i in range(len(lanternfish)):
         if DEBUG:
-            print(f"Day {day} out of {days} had {len(lanternfish)} lanternfish.")
+            print(lanternfish[i], lanternfish[i] - days, (lanternfish[i] - days) // -6, end=" ")
+        lanternfish[i] -= days
+        new_lanternfish: Sequence[int] = (8,) * (lanternfish[i] // -6)
+        if DEBUG:
+            print(new_lanternfish)
+        lanternfish += pass_days_for_new(new_lanternfish, days - 1)
 
     return lanternfish
 
-lanternfish = read_list_from_file()
-lanternfish_after_80 = pass_days(lanternfish, 80)
-lanternfish_after_256 = pass_days(lanternfish, 256)
+def pass_days_for_new(lanternfish_input: Sequence[int], days: int) -> Sequence[int]:
+    global stack
+    stack += 1
+    # print(stack, days)
+    if days < 0:
+        stack -= 1
+        return lanternfish_input
 
-print(f"Number of lanternfish after 80 days: {len(lanternfish_after_80)}")
-# OK I see what you're doing
-print(f"Number of lanternfish after 256 days: {len(lanternfish_after_256)}")
+    lanternfish = [x for x in lanternfish_input]
+    for i in range(len(lanternfish)):
+        if DEBUG and i > 3:
+            print(i, days)
+        # if DEBUG:
+        #     print(lanternfish[i], lanternfish[i] - days, (lanternfish[i] - days) // -6)
+        #     print(f"DAYSS: {days}")
+        lanternfish[i] -= days
+        days -= 1
+        new_lanternfish = (8,) * (lanternfish[i] // -6)
+        lanternfish += pass_days_for_new(new_lanternfish, days)
+
+    stack -= 1
+    return lanternfish
+
+stack = 0
+
+lanternfish = read_list_from_file()
+lanternfish_after_days = {
+    x: pass_days(lanternfish, x) for x in (3, 80)
+}
+
+for days, lanternfish_after in lanternfish_after_days.items():
+    print(f"Number of lanternfish after {days} days: {len(lanternfish)}")
+    print(f" The lanternfish: {lanternfish_after}")
+    # OK I see what you're doing
