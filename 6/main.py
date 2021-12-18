a02041028c850f80c69b00ce0481eb99183d1f3f
SeerLite <seerlite@tuta.io>
2021-12-17 21:35:23 -0300

    Day 6: Try to optimize by mutating single list

--- 6/main.py
+++ 6/main.py
@@ -28,14 +28,22 @@ def print_new_lanternfishes(lanternfishes: Sequence[int], days: int) -> None:
     print(len(sorted(x for x in lanternfishes)))
 
 
-def recurse_lanternfishes(lanternfishes: Sequence[int], days: int) -> list[int]:
-    if not lanternfishes:
-        return list(lanternfishes)
-    lanternfishes = list(lanternfishes)
-    new_lanternfishes: list[int] = []
-    for i in range(len(lanternfishes)):
+def recurse_lanternfishes(lanternfishes: MutableSequence[int], days: int, offset: int = 0) -> MutableSequence[int]:
+    global recursion_level
+    recursion_level += 1
+
+    end = len(lanternfishes)
+
+    if DEBUG:
+        print(recursion_level, end)
+
+    if offset >= end:
+        recursion_level -= 1
+        return lanternfishes
+
+    for i in range(offset, end):
         new_fishes_num = math.ceil((lanternfishes[i] - days) / -7)
-        new_lanternfishes += range(
+        lanternfishes += range(
             # Shift by the distance from 0 (+ 1 but I'm not sure why)
             8 + lanternfishes[i] + 1,
             # Cycle goes for 7 days
@@ -43,17 +51,17 @@ def recurse_lanternfishes(lanternfishes: Sequence[int], days: int) -> list[int]:
             7
         )
 
-    lanternfishes += recurse_lanternfishes(new_lanternfishes, days)
+    recurse_lanternfishes(lanternfishes, days, end)
 
+    recursion_level -= 1
     return lanternfishes
 
-def print_new_lanternfishes_m(lanternfishes: Sequence[int], days: int) -> None:
-    # print(sorted(x for x in recurse_lanternfishes(lanternfishes, days)))
-    print(len(sorted(x for x in recurse_lanternfishes(lanternfishes, days))))
+def print_new_lanternfishes_m(lanternfishes: MutableSequence[int], days: int) -> None:
+    print(len(recurse_lanternfishes(lanternfishes, days)))
 
-
-days = 90
+recursion_level = 0
+days = 150
 lanternfishes = read_list_from_file()
 print(lanternfishes)
-print_new_lanternfishes(lanternfishes, days)
+# print_new_lanternfishes(lanternfishes, days)
 print_new_lanternfishes_m(lanternfishes, days)
