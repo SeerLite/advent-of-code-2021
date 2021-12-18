cb79a45eabd2bfea8b314f2e58c836bda2712715
SeerLite <seerlite@tuta.io>
2021-12-17 23:55:28 -0300

    Day 6: Clean up code a little more

--- 6/main.py
+++ 6/main.py
@@ -1,6 +1,4 @@
-from collections.abc import Sequence, MutableSequence, Iterable
-from typing import Any, Union
-from time import sleep
+from collections.abc import Iterable
 import math
 
 EXAMPLE = False
@@ -16,7 +14,7 @@ def read_list_from_file() -> list[int]:
         return [int(x) for x in input_file.read().strip().split(",")]
 
 
-def recurse_lanternfishes(lanternfishes: Iterable[int], days: int, range_result_cache=None, current_max=0, recursion_level=0) -> int:
+def calculate_lanternfishes(lanternfishes: Iterable[int], days: int, range_result_cache=None, current_max=0, recursion_level=0) -> int:
     recursion_level += 1
 
     if not lanternfishes:
@@ -42,7 +40,7 @@ def recurse_lanternfishes(lanternfishes: Iterable[int], days: int, range_result_
         if next_range in range_result_cache:
             range_result = range_result_cache[next_range]
         else:
-            range_result = recurse_lanternfishes(next_range, days, range_result_cache, current_max, recursion_level)
+            range_result = calculate_lanternfishes(next_range, days, range_result_cache, current_max, recursion_level)
             range_result_cache[next_range] = range_result
         num_lanternfishes += range_result
 
@@ -54,6 +52,8 @@ def recurse_lanternfishes(lanternfishes: Iterable[int], days: int, range_result_
     recursion_level -= 1
     return num_lanternfishes
 
-days = 256
 lanternfishes = read_list_from_file()
-print(recurse_lanternfishes(lanternfishes, days))
+
+for days in (80, 256, 4096):
+    amount = calculate_lanternfishes(lanternfishes, days)
+    print(f"After {days} days, there are {amount} lanternfish.")
