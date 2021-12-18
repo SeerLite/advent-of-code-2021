af2058e6dc6a898bd7fca2b6916a16a5d49c682c
SeerLite <seerlite@tuta.io>
2021-12-18 20:12:36 -0300

    Day 7 (post-solution): Use mathematical summation

--- 7/main.py
+++ 7/main.py
@@ -1,7 +1,9 @@
+# post-solution means I've already seen other people's solutions
+
 from typing import Sequence, Optional
 from collections import defaultdict
 
-EXAMPLE_MODE = True
+EXAMPLE_MODE = False
 DEBUG = False
 
 Expenses = dict[int, int]
@@ -30,12 +32,18 @@ def calculate_cheapest_expense(crabs: Sequence[int], key=lambda pos, crab: abs(p
 
     return min_expense
 
+# NOTE: post-solution
+def summation(n):
+    return n * (n + 1) // 2
+
 crabs = read_input()
 cheapest_expense = calculate_cheapest_expense(crabs)
 
 true_cheapest_expense = calculate_cheapest_expense(
     crabs,
-    key=lambda pos, crab: sum(range(abs(pos - crab) + 1))
+    # NOTE: post-solution
+    key=lambda pos, crab: summation(pos - crab)
+    # key=lambda pos, crab: abs(pos - crab) * (abs(pos - crab) + 1) // 2
 )
 
 print(f"At the most efficient position, the spent fuel is {cheapest_expense}")
