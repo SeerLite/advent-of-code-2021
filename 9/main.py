65980e5d3fab0533a6161a842828be59c0b3d4cf
SeerLite <seerlite@tuta.io>
2021-12-20 01:20:14 -0300

    Day 9: Add full code for part 2

--- 9/main.py
+++ 9/main.py
@@ -1,13 +1,15 @@
-from typing import NamedTuple
+from typing import NamedTuple, Iterable, Sequence, Union
+from copy import deepcopy
+from time import sleep
 
 DEBUG = True
-EXAMPLE_INPUT = True
+EXAMPLE_INPUT = False
 EXPECTED_EXAMPLE_OUTPUT_1 = 15
 EXPECTED_EXAMPLE_OUTPUT_2 = 1134
 output_1 = 0
 output_2 = 0
 
-HeightMap = list[list[int]]
+HeightMap = Sequence[Sequence[int]]
 
 class Coordinate(NamedTuple):
     x: int
@@ -18,7 +20,7 @@ def read_input() -> HeightMap:
     with open(filename) as input_file:
         return [[int(x) for x in line] for line in input_file.read().strip().split("\n")]
 
-def print_map(height_map: HeightMap) -> None:
+def print_map(height_map: Union[HeightMap, Sequence[Sequence[Union[str, int]]]]) -> None:
     print("\n".join("".join(str(x) for x in line) for line in height_map))
 
 def get_neighbors(height_map: HeightMap, coordinate: Coordinate) -> dict[Coordinate, int]:
@@ -44,6 +46,42 @@ def get_low_points(height_map: HeightMap) -> dict[Coordinate, int]:
                 low_points[Coordinate(x, y)] = value
     return low_points
 
+def get_greater_neighbors(height_map: HeightMap, points: Iterable[Coordinate]) -> set[Coordinate]:
+    greater_neighbors = set[Coordinate]()
+    for coordinate in points:
+        for neighbor_coord, neighbor_value in get_neighbors(height_map, coordinate).items():
+            if 9 > neighbor_value > height_map[coordinate.y][coordinate.x]:
+                greater_neighbors.add(neighbor_coord)
+
+    return greater_neighbors
+
+def get_basin_sizes(height_map: HeightMap, points: Iterable[Coordinate]) -> list[int]:
+    sizes: list[int] = []
+    if DEBUG:
+        debug_map: list[list[Union[str, int]]] = [
+            [value for value in row] for row in height_map
+        ]
+
+    for i, coordinate in enumerate(points):
+        size = 1
+        neighbors: Iterable[Coordinate] = [coordinate]
+        prev_neighbors = set[Coordinate]()
+        while (neighbors := get_greater_neighbors(height_map, neighbors)):
+            neighbors -= prev_neighbors
+            prev_neighbors.update(neighbors)
+            size += len(neighbors)
+
+            if DEBUG:
+                for neighbor in neighbors:
+                    debug_map[neighbor.y][neighbor.x] = " "
+                print_map(debug_map)
+                print()
+                if EXAMPLE_INPUT:
+                    sleep(0.2)
+
+        sizes.append(size)
+    return sizes
+
 height_map = read_input()
 
 if DEBUG:
@@ -62,5 +100,17 @@ if EXAMPLE_INPUT and EXPECTED_EXAMPLE_OUTPUT_1 is not None:
 
 print(f"The total risk level is {total_risk_level}")
 
+basin_sizes = get_basin_sizes(height_map, low_points.keys())
+if DEBUG:
+    print(basin_sizes)
+
+largest_basins_product = 1
+for size in sorted(basin_sizes, reverse=True)[:3]:
+    largest_basins_product *= size
+
+output_2 = largest_basins_product
+
 if EXAMPLE_INPUT and EXPECTED_EXAMPLE_OUTPUT_2 is not None:
     assert output_2 == EXPECTED_EXAMPLE_OUTPUT_2,  f"output doesn't match with one from official example: got {output_2}, should be {EXPECTED_EXAMPLE_OUTPUT_2}"
+
+print(f"The product of the three largest basins is {largest_basins_product}")
